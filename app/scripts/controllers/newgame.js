a7bbe738a87e6b9b36ba36fb56ee8a04b21d0516
mordrax <joseph.ni@gmail.com>
2014-08-25 21:57:31 +1000

    fleshing out tactical comman

--- app/scripts/controllers/newgame.js
+++ app/scripts/controllers/newgame.js
@@ -13,10 +13,26 @@ angular.module('battleshipApp')
         $scope.games = sync.$asArray();
 
         $scope.newgame = function(name) {
+            var ships = [];
+            ships.push({name:'speedboat', size:2});
+            ships.push({name:'gun boat', size:3});
+            ships.push({name:'destroyer', size:4});
+            ships.push({name:'battleship', size:5});
+
             $scope.games.$add({
                 name:name,
-                playerOne:{},
-                p2:{}
+                created:Date.now(),
+                winner:null,
+                playerOne:{
+                    ships:ships
+                },
+                p2:{
+                    ships:ships
+                }
             });
         };
+
+        $scope.formatDate = function(date) {
+            return (new Date(date)).toLocaleString();
+        }
     });
