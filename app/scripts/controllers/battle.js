8621172658913ec8acfa31f923af77e9c74ad9b6
mordrax <joseph.ni@gmail.com>
2014-08-30 16:02:20 +1000

    adding ships joining in as a player

--- app/scripts/controllers/battle.js
+++ app/scripts/controllers/battle.js
@@ -8,16 +8,34 @@
  * Controller of the battleshipApp
  */
 angular.module('battleshipApp')
-    .controller('BattleCtrl', function ($scope, $firebase) {
+    .controller('BattleCtrl', function ($scope, $firebase, $cookieStore, CurrentGame) {
         var sync = $firebase(new Firebase('https://glowing-fire-6509.firebaseio.com/games'));
-        $scope.playerGrid = [1,2,3,4,5,6,7,8];
+        var gameKey = $cookieStore.get('battleship_currentGameKey');
+        var ref = $firebase(new Firebase('https://glowing-fire-6509.firebaseio.com/games/'+gameKey));
+        $scope.player = $cookieStore.get('battleship_currentPlayer');
+        $scope.game = ref.$asObject();
 
-        $scope.ready = (function()
-        {
-            $("td").bind("click", function(){
-              alert( $(this).text() );
-              // background changes to red
-              $(this).addClass("clicked");
-          });
-        });
+        $scope.placeShip = function(ship) {
+            $scope.placingShip = ship;
+            $scope.gameState = "Currently placing your " + ship.name;
+        };
+
+        $scope.gameState = "Welcome to battleship, please place your ships.";
+
+        $scope.clickCell = function (cell) {
+            if (!$scope.placingShip) {
+                $scope.gameState = "Please select a ship to place!!!!!!!!!";
+                return;
+            }
+
+            if (cell.x + $scope.placingShip.size >= CurrentGame.GRID_SIZE) {
+                $scope.gameState = "Please place your ship within the game boundaries.";
+            } else {
+                var ship = $scope.placingShip;
+                for (var i=0; i<ship.size; i++) {
+                    ship.coords[i] = {x: cell.x+i, y:cell.y};
+                }
+                ref.$update($scope.player, angular.copy($scope.game[$scope.player]));
+            }
+        }
     });
--- app/scripts/controllers/newgame.js
+++ app/scripts/controllers/newgame.js
@@ -8,16 +8,24 @@
  * Controller of the battleshipApp
  */
 angular.module('battleshipApp')
-    .controller('NewgameCtrl', function ($scope, $firebase) {
+    .controller('NewgameCtrl', function ($scope, $firebase, $location, CurrentGame, $cookieStore) {
         var sync = $firebase(new Firebase('https://glowing-fire-6509.firebaseio.com/games'));
-        $scope.games = sync.$asArray();
 
+        $scope.games = sync.$asArray();
         $scope.newgame = function(name) {
             var ships = [];
-            ships.push({name:'speedboat', size:2});
-            ships.push({name:'gun boat', size:3});
-            ships.push({name:'destroyer', size:4});
-            ships.push({name:'battleship', size:5});
+            ships.push({name:'speedboat', size:2, health:2, coords:{state:'none'}});
+            ships.push({name:'gun boat', size:3, health:3, coords:{state:'none'}});
+            ships.push({name:'destroyer', size:4, health:4, coords:{state:'none'}});
+            ships.push({name:'battleship', size:5, health:5, coords:{state:'none'}});
+
+            var grid = {};
+            for (var row = 0; row<=CurrentGame.GRID_SIZE; row++) {
+                grid[row] = {};
+                for ( var col = 0; col<= CurrentGame.GRID_SIZE; col++) {
+                    grid[row][col] = {y:row,x:col};
+                }
+            }
 
             $scope.games.$add({
                 name:name,
@@ -28,11 +36,23 @@ angular.module('battleshipApp')
                 },
                 p2:{
                     ships:ships
-                }
+                },
+                grid: grid
             });
         };
 
         $scope.formatDate = function(date) {
             return (new Date(date)).toLocaleString();
+        };
+
+        $scope.delete = function(game) {
+            $scope.games.$remove(game);
+        };
+
+        $scope.join = function(game) {
+            CurrentGame.setGame(game);
+            $cookieStore.put('battleship_currentGameKey', $scope.games.$keyAt(game));
+            $cookieStore.put('battleship_currentPlayer', 'playerOne');
+            $location.path("/battle");
         }
     });
